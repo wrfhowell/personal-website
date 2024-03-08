e459e8664d778464caa88f097cbd87be5afd9063
Will Howell <wrfhowell@gmail.com>
2024-03-08 15:10:28 -0800

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Resume from "./Resume";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-export const base = "/personal-website/";
+export const base = "/";
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
